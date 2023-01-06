e1c121e590ee11d4c3450abf610f19779c25db9d
Kanmani Thiagarajan <kanmani@Kanmanis-MacBook-Air.local>
2023-01-05 19:44:46 -0800

    Bandnamegenerator generates band name from city and pet name.

---
+++ d01/bandnamegenerator.py
@@ -0,0 +1,9 @@
+#1. Create a greeting for your program.
+print("Welcome to the Band Name Generator.")
+#2. Ask the user for the city that they grew up in.
+City=input("What is the name of the city you grew up in?\n")
+#3. Ask the user for the name of a pet.
+Pet_name=input("What is name of your pet?\n")
+#4. Combine the name of their city and pet and show them their band name.
+Band_name=print("Your Band Name Could be " + City + " " + Pet_name)
+#5. Make sure the input cursor shows on a new line:
